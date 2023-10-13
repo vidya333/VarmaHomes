de45f73746b20c50179008def679c030a8a0cd58
Vidya <vidya.nk07@gmail.com>
2023-10-13 23:37:28 +0530

    1

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+// CARD SLIDER NEXT PREV BUTTONS
+var sliderMain = document.getElementById("slider-main");
+var item = document.getElementsByClassName("item");
+
+function next(){
+    sliderMain.append(item[0]);
+}
+function prev(){
+    sliderMain.prepend(item[item.length-1]);
+}
+
+
+// BACK TO TOP BUTTON
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
